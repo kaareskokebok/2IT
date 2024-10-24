af86334b1fd37f1085bb3c5e4dcc22242a8132a6
kaareskokebok <kaare@jdforlag.no>
2024-10-24 09:15:53 +0200

    update

--- JavaScript/Del1/Ssp-funksjoner/script.js
+++ JavaScript/Del1/Ssp-funksjoner/script.js
@@ -3,7 +3,15 @@
 // f.eks. er steinKnapp = ... ikke tillatt
 
 // Funksjoner i toppen
+function cpuValg() {
+    const valg = ["Stein", "Saks", "Papir"];
+    // Math.random(), gir tilfeldig flyttall 0-1
+    // Math.floor(), runder ned til nærmeste heltall
+    // TODO: lag tilfeldig heltall 0, 1, eller 2
+    const index = 
 
+
+}
 
 const steinKnapp = document.getElementById("stein");
 const saksKnapp = document.getElementById("saks");
