ebfa41e3c99db8c50b4887befc427a8a9e5f3516
kaareskokebok <kaare@jdforlag.no>
2024-10-24 09:06:36 +0200

    add ssp

---
+++ JavaScript/Del1/Ssp-funksjoner/script.js
@@ -0,0 +1,13 @@
+// console.log("test");
+// const betyr constant: variabelen kan ikke endres senere
+// f.eks. er steinKnapp = ... ikke tillatt
+
+// Funksjoner i toppen
+
+
+const steinKnapp = document.getElementById("stein");
+const saksKnapp = document.getElementById("saks");
+const papirKnapp = document.getElementById("papir");
+const resultat = document.getElementById("txtUt");
+
+resultat.textContent = "test";
