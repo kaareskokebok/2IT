b22b85bffe1d995896c6ff434ae506282a04ea77
kaareskokebok <kaare@jdforlag.no>
2024-10-30 12:46:07 +0100

    add txtUt-felt

--- JavaScript/Del2-objekter/bibliotek/script.js
+++ JavaScript/Del2-objekter/bibliotek/script.js
@@ -3,12 +3,13 @@ function sokBok(){
     // 1. Les inn bok fra input-felt
     // 2. Sjekk om boktittelen i input-feltet matcher boka i
     // biblioteket (abc-boka)
-    // 3. Gi tilbakemelding
+    // 3. Gi tilbakemelding i txtUt-elementet
 }
 
 let bok = "abc-boka";
 
 let inpSokBok = document.getElementById("inpSokBok");
 let btnSokBok = document.getElementById("btnSokBok");
+let txtUt = document.getElementById("txtUt");
 
 btnSokBok.addEventListener("click", sokBok);
