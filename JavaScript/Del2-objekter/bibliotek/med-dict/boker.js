d75fb835a52b2b2255a1bdf1b2c63bcc4a493087
kaareskokebok <kaare@jdforlag.no>
2024-10-30 14:22:51 +0100

    add visboker og css

---
+++ JavaScript/Del2-objekter/bibliotek/med-dict/boker.js
@@ -0,0 +1,170 @@
+let boker = [
+    {
+        tittel: "Jenserns Gensere",
+        forfatter: "Jensern",
+        sider: 497,
+        beskrivelse: "Geniale strikkeoppskrifter for ullgensere",
+        antall: 3,
+        aar: 2009
+    },
+    {
+        tittel: "Mysteriet i Skogen",
+        forfatter: "Kari Nordmann",
+        sider: 320,
+        beskrivelse: "En spennende fortelling om en forsvunnet turgåer.",
+        antall: 5,
+        aar: 2018
+    },
+    {
+        tittel: "Historier fra Havet",
+        forfatter: "Lars Hansen",
+        sider: 280,
+        beskrivelse: "Sjømannsfortellinger og eventyr fra de syv hav.",
+        antall: 2,
+        aar: 2015
+    },
+    {
+        tittel: "Den Hemmelige Hagen",
+        forfatter: "Maria Lind",
+        sider: 250,
+        beskrivelse: "En magisk hage full av overraskelser.",
+        antall: 4,
+        aar: 2012
+    },
+    {
+        tittel: "Matglede for Alle",
+        forfatter: "Olav Kokkeland",
+        sider: 150,
+        beskrivelse: "Enkle og deilige oppskrifter for hele familien.",
+        antall: 6,
+        aar: 2020
+    },
+    {
+        tittel: "Fjelltoppene Kaller",
+        forfatter: "Ingrid Berg",
+        sider: 340,
+        beskrivelse: "En guide til Norges vakreste fjellturer.",
+        antall: 3,
+        aar: 2019
+    },
+    {
+        tittel: "Teknologiens Tidsalder",
+        forfatter: "Per Olsen",
+        sider: 400,
+        beskrivelse: "Hvordan teknologi former vår fremtid.",
+        antall: 7,
+        aar: 2021
+    },
+    {
+        tittel: "Barnas Beste Eventyr",
+        forfatter: "Astrid Moen",
+        sider: 220,
+        beskrivelse: "En samling av klassiske eventyr for barn.",
+        antall: 5,
+        aar: 2010
+    },
+    {
+        tittel: "Språkets Magi",
+        forfatter: "Erik Dahl",
+        sider: 310,
+        beskrivelse: "En utforskning av språk og kommunikasjon.",
+        antall: 2,
+        aar: 2016
+    },
+    {
+        tittel: "Den Lille Kokeboken",
+        forfatter: "Camilla Sørensen",
+        sider: 180,
+        beskrivelse: "Kjappe og enkle retter for travle hverdager.",
+        antall: 8,
+        aar: 2013
+    },
+    {
+        tittel: "Reisen til Nordpolen",
+        forfatter: "Hans Pettersson",
+        sider: 360,
+        beskrivelse: "En ekspedisjon til jordens nordligste punkt.",
+        antall: 4,
+        aar: 2005
+    },
+    {
+        tittel: "Kunstens Historie",
+        forfatter: "Elisabeth Nilsen",
+        sider: 420,
+        beskrivelse: "En gjennomgang av kunst fra antikken til i dag.",
+        antall: 6,
+        aar: 1998
+    },
+    {
+        tittel: "Musikkens Verden",
+        forfatter: "Thomas Eriksen",
+        sider: 270,
+        beskrivelse: "En reise gjennom musikkhistorien.",
+        antall: 3,
+        aar: 2001
+    },
+    {
+        tittel: "Dyrene i Afrika",
+        forfatter: "Solveig Karlsen",
+        sider: 290,
+        beskrivelse: "En bok om Afrikas fascinerende dyreliv.",
+        antall: 5,
+        aar: 2017
+    },
+    {
+        tittel: "Romfartens Pionerer",
+        forfatter: "Jan Andersen",
+        sider: 330,
+        beskrivelse: "Historien om menneskets reise til verdensrommet.",
+        antall: 2,
+        aar: 2014
+    },
+    {
+        tittel: "Den Usynlige Fienden",
+        forfatter: "Linda Berg",
+        sider: 350,
+        beskrivelse: "En thriller om cyberkriminalitet.",
+        antall: 7,
+        aar: 2019
+    },
+    {
+        tittel: "Hagearbeid for Alle",
+        forfatter: "Grethe Dahl",
+        sider: 240,
+        beskrivelse: "Tips og triks for en frodig hage.",
+        antall: 6,
+        aar: 2008
+    },
+    {
+        tittel: "Verdens Beste Vitser",
+        forfatter: "Ole Latter",
+        sider: 200,
+        beskrivelse: "En samling av morsomme vitser og historier.",
+        antall: 9,
+        aar: 2011
+    },
+    {
+        tittel: "Fotografiets Kunst",
+        forfatter: "Henrik Lunde",
+        sider: 380,
+        beskrivelse: "Teknikker og inspirasjon for fotografer.",
+        antall: 4,
+        aar: 2003
+    },
+    {
+        tittel: "Vinens Verden",
+        forfatter: "Anna Vinje",
+        sider: 260,
+        beskrivelse: "En guide til vinens historie og smaker.",
+        antall: 5,
+        aar: 2018
+    },
+    {
+        tittel: "Yoga for Livet",
+        forfatter: "Marte Solberg",
+        sider: 310,
+        beskrivelse: "En innføring i yoga og meditasjon.",
+        antall: 3,
+        aar: 2015
+    }
+];
--- JavaScript/Del2-objekter/bibliotek/med-dict/script.js
+++ JavaScript/Del2-objekter/bibliotek/med-dict/script.js
@@ -1,10 +1,3 @@
-// Array-funksjoner: 
-// array.remove("boktittel")
-// array.push("boktittel"), legge til bok
-// array.includes("boktittel")
-// array.indexOf("boktittel")
-
-
 function sokBok(){
     // TODO:
     // 1. Les inn bok fra input-felt
@@ -27,34 +20,32 @@ function laanBok(){
     // Tips: ligner på søkeboklet sokebok = inpSokBok.value;
     // 2. Sjekk om boktittelen i input-feltet en av bøkene i 
     // biblioteket
-    if (boker.includes(sokebok)) {
-        let index = boker.indexOf(sokebok);
-        boker.splice(index, 1);  // slett 1 bok på plass index
-
-        txtUt.textContent = "Boka ble utlånt. ";
-    } else{
-        txtUt.textContent = "Boka finnes ikke";
-    }
-    console.log(boker);
-    // Fjern fra array dersom den finnes
-    // Gi tilbakemelding
+   
 }
 
 function leverBok(){
-    // TODO: legg til boka i arrayet bøker
-    // Gi tilbakemelding
-    let sokebok = sokBok.value;
-    boker.push(sokebok);  // Legges til i slutten av arrayet
+    // TODO: 
 
-    txtUt.textContent = "Takk for at du leverte boka";
 }
 
-let boker = ["abc-boka", "def-boka", "Norske Fjell", "Q", "Yo-yo"];
+function visBoker(){
+    // Jo mere vi er sammen...
+    let txt = "<h3>Oversikt bøker</h3><hr>";
+    txt += "<ul>";
+    for (bok of boker) {
+        txt += `<li>${bok.tittel}</li>`;
+    }
+    txt += "</ul>";
+
+    txtUt.innerHTML = txt;
+}
 
 let inpSokBok = document.getElementById("inpSokBok");
 let btnSokBok = document.getElementById("btnSokBok");
 let txtUt = document.getElementById("txtUt");
-
+let btnVisBoker = document.getElementById("btnVisBoker");
 btnSokBok.addEventListener("click", sokBok);
 btnLaanBok.addEventListener("click", laanBok);
 btnLeverBok.addEventListener("click", leverBok);
+btnVisBoker.addEventListener("click", visBoker);
+
