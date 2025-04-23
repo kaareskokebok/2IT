509d338f13bf182bff397891dd3917c9ed91177b
kaareskokebok <kaare@jdforlag.no>
2025-04-23 13:21:30 +0200

    skjema test

--- rep-eksamen/nodejs-rep/2-paamelding/app.js
+++ rep-eksamen/nodejs-rep/2-paamelding/app.js
@@ -1,2 +1,22 @@
 // nettsiden skal ha et skjema der brukeren kan skrive inn antall personer
 // får tilbake en enkel kvittering med antall personer og pris (150,- stykk)
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+// For å lese fra skjema med req.body
+app.use(express.urlencoded({ extended:true }));
+
+
+app.post("/paamelding", (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server kjører på port ${PORT}.`)
+    console.log(`http://localhost:${PORT}`);
+});
+
+
